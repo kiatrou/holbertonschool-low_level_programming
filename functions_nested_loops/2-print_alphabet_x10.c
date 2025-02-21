5797939051b7ac736e57f998d080995345aaa65a
Kassandra <kassandraiatrou@gmail.com>
2025-02-21 08:35:55 +0000

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+*/
+void print_alphabet_x10(void)
+{
+	int num = 0;
+	char letter;
+
+	while (num++ <= 9)
+	{	
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	}
+	_putchar('\n');
+}
