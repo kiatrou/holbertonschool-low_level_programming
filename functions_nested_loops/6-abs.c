96af8636330ce1384f283cc16d5f0282a6a78e48
Kassandra <kassandraiatrou@gmail.com>
2025-02-21 10:13:19 +0000

    task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _abs - print the absolute vaulue of an integer
- * @int: integer being checked
+ * @n: integer being checked
  *
  * Return: If int is lower than zero it returns the int in a negative,
  * else it returns the int
