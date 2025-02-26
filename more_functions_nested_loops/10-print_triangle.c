e24fb37384c83dc2ae0de490402c4d302298cd89
Kassandra <kassandraiatrou@gmail.com>
2025-02-26 03:01:15 +0000

    task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle according to
+ * number of #
+ * @size: size of triangle using #
+ *
+*/
+void print_triangle(int size)
+{
+	int height = 0;
+	int width = 0;
+	int tri = 0;
+
+	if (size > 0)
+	{
+		while (height < size)
+		{
+			width = 0;
+
+			while (width < size - height - 1)
+			{
+				_putchar(' ');
+				width++;
+			}
+			tri = 0;
+
+			while (tri <= height)
+			{
+				_putchar('#');
+				tri++;
+			}
+			_putchar('\n');
+			height++;
+		}
+	}
+}
