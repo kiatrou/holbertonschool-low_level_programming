8c24267568212de03d5896eace58723bcfd1fd9e
Kassandra <kassandraiatrou@gmail.com>
2025-02-27 10:44:44 +0000

    task 5 reverses a string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,29 @@
 */
 void rev_string(char *s)
 {
-	char *end = s;
+	char *start = s; /*Start of the string*/
+	char *end = s; /*End of the string*/
 
-	while (*end != '\0') /*This loop counts to the end of the string*/
+	while (*end != '\0') /*Traverses the string from start to finish*/
 	{
 		end++;
 	}
 
-	while (end != s) /*This loop prints the string in reverse*/
+	end--; /*Goes back a space so end doesn't start on NULL*/
+
+	while (start < end)
+	/**
+	 * This loop runs as long as start is less than end and stops
+	when the two points meet in the middle
+	*/
 	{
+		char temp = *start; /*Creates a temporary space for the start variable*/
+		*start = *end;
+		*end = temp;
+
+		start++; /*Starts counting forward from the start of the string*/
 		end--;
-		_putchar(*end);
+		/*Starts counting from the end of the string and moves towards the start*/
 	}
 	_putchar('\n');
 }
