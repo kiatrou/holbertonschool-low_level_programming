f52a228987149def4e7de97468252f1a03eacb4e
Kassandra <kassandraiatrou@gmail.com>
2025-02-27 03:40:02 +0000

    started project 2 on pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - this resets the value of the variable
+ * n using a pointer to 98
+ * @n: pointer to the variable address n
+ *
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
