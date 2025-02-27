23c647b5d49c4b559f26d8f2bbfd7f9a5997dd3b
Kassandra <kassandraiatrou@gmail.com>
2025-02-27 05:02:05 +0000

    task 3 prints a string to the standard output using _putchar

---
+++ pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints a string
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
