98e59af16fa4cf9454fe25f63ce7c3f41ad28cc3
Kassandra <kassandraiatrou@gmail.com>
2025-03-01 05:16:49 +0000

    task 7 encodes a string into 1337 aka leetspeak

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * leet - encodes a string into
+ * 1337 (leetspeak)
+ * @s: string being encoded
+ *
+ * Return: encoded string
+*/
+char *leet(char *s)
+{
+	int count = 0;
+
+	/*Loop through string until NULL*/
+	while (s[count] != '\0')
+	{
+		/*Check if char is specified letter*/
+		if (s[count] == 'a' || s[count] == 'A')
+		{
+			s[count] = '4';
+		}
+		else if (s[count] == 'e' || s[count] == 'E')
+		{
+			s[count] = '3';
+		}
+		else if (s[count] == 'o' || s[count] == 'O')
+		{
+			s[count] = '0';
+		}
+		else if (s[count] == 't' || s[count] == 'T')
+		{
+			s[count] = '7';
+		}
+		else if (s[count] == 'l' || s[count] == 'L')
+		{
+			s[count] = '1';
+		}
+		count++;
+	}
+	return (s);
+}
