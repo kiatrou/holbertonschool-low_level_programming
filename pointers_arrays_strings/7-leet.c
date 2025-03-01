ae951bf4a24dcc94a7b22e1f1e64d7ef4a26be11
Kassandra <kassandraiatrou@gmail.com>
2025-03-01 05:30:10 +0000

    task 7 encodes a string into 1337 aka leetspeak

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -13,26 +13,34 @@ char *leet(char *s)
 	/*Loop through string until NULL*/
 	while (s[count] != '\0')
 	{
-		/*Check if char is specified letter*/
-		if (s[count] == 'a' || s[count] == 'A')
+		
+		if (s[count] == 'a' || s[count] == 'A' ||
+		s[count] == 'e' || s[count] == 'E' || s[count]
+		== 'o' || s[count] == 'O' || s[count] == 't' ||
+		s[count] == 'T' || s[count] == 'l' || s[count]
+		== 'L')
 		{
-			s[count] = '4';
-		}
-		else if (s[count] == 'e' || s[count] == 'E')
-		{
-			s[count] = '3';
-		}
-		else if (s[count] == 'o' || s[count] == 'O')
-		{
-			s[count] = '0';
-		}
-		else if (s[count] == 't' || s[count] == 'T')
-		{
-			s[count] = '7';
-		}
-		else if (s[count] == 'l' || s[count] == 'L')
-		{
-			s[count] = '1';
+			/*Check if char is specified letter*/
+			if (s[count] == 'a' || s[count] == 'A')
+			{
+				s[count] = '4';
+			}
+			else if (s[count] == 'e' || s[count] == 'E')
+			{
+				s[count] = '3';
+			}
+			else if (s[count] == 'o' || s[count] == 'O')
+			{
+				s[count] = '0';
+			}
+			else if (s[count] == 't' || s[count] == 'T')
+			{
+				s[count] = '7';
+			}
+			else if (s[count] == 'l' || s[count] == 'L')
+			{
+				s[count] = '1';
+			}
 		}
 		count++;
 	}
