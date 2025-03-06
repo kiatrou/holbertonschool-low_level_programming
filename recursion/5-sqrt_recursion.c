cc34d6d9f0b4c37186316484f051af762ec63654
Kassandra <kassandraiatrou@gmail.com>
2025-03-06 05:49:51 +0000

    task 5 returns the natural square root of a number and betty is being a headache

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - initiates the process of
+ * finding the natural square root of a given
+ * number.
+ * sqrt_helper - this function keeps track
+ * of the current base for the sqaure root
+ * of n. It controls how the trial and error
+ * works and keeps updating base until it
+ * either finds a square root or exceeds.
+ * @n: number we are finding the square root of
+ * @base: current number being tested to see if
+ * its square equals the given number n.
+ * @n: given number
+ *
+ * Return: (recur)if equal to n, return square root
+ * if greater than n, return -1 because it is
+ * not a natural square root
+ * Returns: (helper)Recurses next guess
+*/
+int sqrt_helper(int n, int base)
+{
+	/*Return base as square root*/
+	if (base * base == n)
+	{
+		return (base);
+	}
+
+	/*If exceeds n, return -1*/
+	if (base * base > n)
+	{
+		return (-1);
+	}
+
+	/*Recursive, tries the next num*/
+	return (sqrt_helper(n, base + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt_helper(n, 1));
+}
