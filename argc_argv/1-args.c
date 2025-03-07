c2a3bf7548f1812ec9d2b13edf31bd58aebc8eda
Kassandra <kassandraiatrou@gmail.com>
2025-03-07 03:25:10 +0000

    task 1 is a program that prints the number of arguments passed into it

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments
+ * passed to it
+ * @argc: integer number that stores the number
+ * of command-line arguments passed by the user
+ * @argv: unused
+ *
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
