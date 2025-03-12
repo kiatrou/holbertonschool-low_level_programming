80ca1bac51e38d55425428da9548ec60db84074f
Kassandra <kassandraiatrou@gmail.com>
2025-03-13 10:44:08 +1100

    Update 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -22,11 +22,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s2_count;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
+	
 	while (s1[s1_len] != '\0')
 	{
 		s1_len++;
@@ -35,23 +38,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2_len++;
 	}
+	
 	if (n >= s2_len)
 	{
 		n = s2_len;
 	}
+	
 	new_str = malloc(s1_len + n + 1);
+	
 	if (new_str == NULL)
 	{
 		return (NULL);
 	}
+	
 	for (s1_count = 0; s1_count < s1_len; s1_count++)
 	{
 		new_str[s1_count] = s1[s1_count];
 	}
+	
 	for (s2_count = 0; s2_count < n; s2_count++)
 	{
 		new_str[s1_len + s2_count] = s2[s2_count];
 	}
+	
 	new_str[s1_len + n] = '\0';
+	
 	return (new_str);
 }
