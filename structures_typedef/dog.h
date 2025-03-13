da26db3064a96d3f107440e5a6033f86c5a1c81b
Kassandra <kassandraiatrou@gmail.com>
2025-03-13 07:37:31 +0000

    task 0 - define/create a new structure (struct) called dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - elements for dog struct
+ * @name: pointer to dog's name
+ * @age: dog's age
+ * @owner: pointer to dog owner
+ *
+ * Description: This struct holds elements
+ * relating to the information of a dog.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
