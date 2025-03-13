7c44711de0ec3d2d527cdfc5e83490361e6580b2
Kassandra <kassandraiatrou@gmail.com>
2025-03-13 09:00:55 +0000

    task 2 is a function that prints struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints the elements from
+ * struct dog
+ * @d: pointer to struct dog
+ *
+ * Return: none
+*/
+void print_dog(struct dog *d)
+{
+	/*If pointer is NULL, do nothing*/
+	if (d == NULL)
+	{
+		return;
+	}
+
+	/*Print name, if NULL print (nil)*/
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	/*Print age, if 0.0 (because float) print (nil)*/
+	if (d->age == 0.0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %.1f\n", d->age);
+	}
+
+	/*Print owner, if NULL print (nil)*/
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
