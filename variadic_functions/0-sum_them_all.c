13ed860f7515e4535c306c9f7b8e007ad0b514d0
Kassandra <kassandraiatrou@gmail.com>
2025-03-20 03:10:38 +0000

    working on task 1 of project 2

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -16,12 +16,20 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		return (0);
 	}
-
 	va_start(args, n);
 	for (count = 0; count < n; count++)
 	{
 		sum += va_arg(args, int);
 	}
 	va_end(args);
+	/*ends traversal of variadic function arguments*/
 	return (sum);
 }
+/**
+ * va_list - defines the variable, in this case 'args'
+ * va_start - enables access to the variadic function
+ * arguments
+ * va_args - this returns the next argument from the list
+ * and must be used repeatedly to access all arguments.
+ * Specifying int means you're specifying the type of
+ * argument to retrieve
---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ *
+ *
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int count; /*Count through args*/
+	char separator = " ,";
+
+	if (separator == NULL)
+	{
+		
+	}
+}
