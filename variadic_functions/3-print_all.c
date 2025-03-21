56c0070af1092ff453fe774c1bf7ff0b5e461924
Kassandra <kassandraiatrou@gmail.com>
2025-03-21 03:14:19 +0000

    task 3 prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -28,21 +28,20 @@ void print_all(const char * const format, ...)
 			c = va_arg(args, int);
 			printf("%c", c);
 			break;
+
 			case 'i':
 			i = va_arg(args, int);
 			printf("%d", i);
 			break;
+
 			case 'f':
 			f = va_arg(args, double);
 			printf("%f", f);
 			break;
+
 			case 's':
 			s = va_arg(args, char *);
-			if (s == NULL)
-			{
-				printf("(nil)");
-			}
-			printf("%s", s);
+			printf("%s", s ? s : "(nil)");
 			break;
 			default:
 			break;
