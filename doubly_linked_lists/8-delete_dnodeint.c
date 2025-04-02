2a5dfb2a99660e05a5eb7cb6c19ed422a3f8dbbf
Kassandra <kassandraiatrou@gmail.com>
2025-04-02 05:55:53 +0000

    task 8 deletes a node at index of a linked list

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -16,11 +16,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
+	current = *head;
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		*head = current->next;
 		if (*head != NULL)
+		{
 			(*head)->prev = NULL;
+			free(current);
+		}
 		return (1);
 	}
 	current = *head;
