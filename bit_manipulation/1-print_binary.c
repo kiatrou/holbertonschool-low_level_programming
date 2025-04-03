91743d6a287e9798beca6a33cd185114c29207a0
Kassandra <kassandraiatrou@gmail.com>
2025-04-03 04:23:51 +0000

    started working on task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - print the binary representation of
+ * a number
+ * @n: number being represented
+*/
+void print_binary(unsigned long int n)
+{
+	if (n == '1'')
+	{
+		n = << 1;
+	}
+}
