7272f1f2c7d7b0d571bcc0f5502933fb84dda49b
Kassandra <kassandraiatrou@gmail.com>
2025-04-03 23:21:15 +0000

    task 1 prints the binary representation of a number

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -7,8 +7,31 @@
 */
 void print_binary(unsigned long int n)
 {
-	if (n == '1'')
+	int count;
+	int size = sizeof(n) * 8;
+	/*Number of bits in an unsigned int*/
+	int started = 0;
+	/*To avoid leading zeros*/
+
+	/*Loop from most significant bit and least significant*/
+	for (count = size - 1; count >= 0; count--)
 	{
-		n = << 1;
+		/*Extract the bit at position count*/
+		if ((n >> count) & 1)
+		{
+			_putchar('1');
+			started = 1;
+			/*Mark that we've found the first 1*/
+		}
+		/*Only print 0s after first 1*/
+		else if (started)
+		{
+			_putchar('0');
+		}
+	}
+	/*If n is 0, just print 0*/
+	if (!started)
+	{
+		_putchar('0');
 	}
 }
